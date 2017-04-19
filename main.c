af95ff5269a3af9cb2676278f2dd62c356fea906
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-04-19 23:55:44 +0530

    Init Working

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+#include <stdlib.h>
 #include <GL/glut.h>
-void main()
+void display(void)
 {
-	
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  
+  glFlush();
+  glutSwapBuffers();
+}
+
+
+void main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("Spin a color cube");
+	glEnable(GL_DEPTH_TEST);
+	glColor3f(1.0,1.0,1.0);
+	glutMainLoop();
 }
