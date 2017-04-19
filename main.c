d3c309bb47a79a9c86d42eeb0d5d2ec3bc01343e
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-04-20 00:04:57 +0530

    Init Working transparent bg resolved

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include <stdlib.h>
 #include <GL/glut.h>
+
+
 void display(void)
 {
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-  
+  glLoadIdentity();
   glFlush();
   glutSwapBuffers();
 }
-
 
 void main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize(500,500);
-	glutCreateWindow("OpenGlNature");
+	glutInitWindowSize(1366,768);
+	glutCreateWindow("Spin a color cube");
+	glutDisplayFunc(display);
 	glEnable(GL_DEPTH_TEST);
 	glColor3f(1.0,1.0,1.0);
 	glutMainLoop();
