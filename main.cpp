962e53b9c39177f86a4d18c02587eb168abb9f3a
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-05-05 10:07:34 +0530

    ready 'migrate to mac

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 
 //default height and width of screen
-#define DEFAULT_WIDTH 1366
+#define DEFAULT_WIDTH 1000
 #define DEFAULT_HEIGHT 768
 int height = DEFAULT_HEIGHT;
 int width = DEFAULT_WIDTH;
@@ -14,14 +14,23 @@ int width = DEFAULT_WIDTH;
 
 
 //define colors used
-GLfloat  SkyBlue[][3]={0.196078,0.6,0.8};
+GLfloat  SkyBlue[3]={0.196078,0.6,0.8};
+GLfloat  GrassColor[3]={0.196078,0.8,0.19};
 
+void grass()
+{
+	glColor3fv(GrassColor);
+	glBegin(GL_POLYGON);
 
+	glEnd();
+
+}
 void display(void)
 {
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
   glFlush();
-  glClearColor(SkyBlue[0][0],SkyBlue[0][1],SkyBlue[0][2],1);
+  glClearColor(SkyBlue[0],SkyBlue[1],SkyBlue[2],1);
+  grass();
   glutSwapBuffers();
 }
 
